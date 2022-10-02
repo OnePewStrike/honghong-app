aab7929177700737c362a1057f3e5cbcc3d5b51c
Godwin Duliente <rualgodwin@gmail.com>
2022-10-02 13:27:30 +0800

    init register, login, and about page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <!-- Header Section  -->
+  <section class="header login">
+    <div class="left-content">
+      <a href="index.html">
+        <h3 class="logo">Honghong</h3>
+      </a>
+    </div>
+  </section>
+
+  <!-- Login Section  -->
+  <section class="hero">
+    <div class="wrapper">
+      <div class="form signup">
+        <header>Login to Honghong</header>
+        <form action="#">
+          <div class="field input">
+            <label for="username">Anonymous Name*</label>
+            <input type="text" placeholder="Enter your anonymous name">
+          </div>
+          <div class="field input">
+            <label for="password">Password*</label>
+            <input type="text" placeholder="Enter your password">
+          </div>
+          <div class="link">Don't have an account? <a href="register.html"> Register now </a></div>
+          <div class="action">
+            <button type="submit" class="btn btn-border">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer Section -->
+  <section class="footer-login">
+
+  </section>
+
+</body>
+</html>
